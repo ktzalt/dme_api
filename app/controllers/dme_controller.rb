b066b757ac3d15c999c5d40ea585b2f25527001e
slbrtg <slbrtg@gmail.com>
2018-04-30 17:00:03 -0700

    add methods to create google, godaddy, and network solutions mx records

--- app/controllers/dme_controller.rb
+++ app/controllers/dme_controller.rb
@@ -20,12 +20,6 @@ class DmeController < ApplicationController
     dme.create_mx_record(domain, key, priority, value, options)
   end
 
-  # Creates the default ANAME and CNAME records for all zones
-  def create_default_records(mx_type)
-    create_record(params[:domain], '', 'ANAME', 'www', {})
-    create_record(params[:domain], 'www', 'CNAME', 'lb.moxiworks.com', {})
-  end
-
   # Creates Google MX and CNAME records 
   def create_gmail_records
     create_mx_record(params[:domain], '', '1', 'aspmx.l.google.com.', {} )
@@ -38,13 +32,60 @@ class DmeController < ApplicationController
     create_record(params[:domain], 'docs', 'CNAME', 'ghs.google.com.', {})
     create_record(params[:domain], 'mail', 'CNAME', 'ghs.google.com.', {})
     create_record(params[:domain], 'start', 'CNAME', 'ghs.google.com.', {})
-    create_record(params[:domain], 'www', 'CNAME', 'ghs.google.com.', {})
+    create_record(params[:domain], 'www', 'CNAME', "#{params[:domain]}.", {})
   end
+
+  #Creates GoDaddy MX and CNAME records
+  def create_godaddy_records
+    create_mx_record(params[:domain], '', '0', 'smtp.secureserver.net.', {} )
+    create_mx_record(params[:domain], '', '5', 'mailstore1.secureserver.net.', {} )
+
+    create_record(params[:domain], 'email', 'CNAME', 'email.secureserver.net.', {})
+    create_record(params[:domain], 'imap', 'CNAME', 'imap.secureserver.net.', {})
+    create_record(params[:domain], 'mail', 'CNAME', 'pop.secureserver.net.', {})
+    create_record(params[:domain], 'mobilemail', 'CNAME', 'mobilemail-v01.prod.mesa1.secureserver.net.', {})
+    create_record(params[:domain], 'pda', 'CNAME', 'mobilemail-v01.prod.mesa1.secureserver.net.', {})
+    create_record(params[:domain], 'pop', 'CNAME', 'pop.secureserver.net.', {})
+    create_record(params[:domain], 'smtp', 'CNAME', 'smtp.secureserver.net.', {})
+    create_record(params[:domain], 'webmail', 'CNAME', 'webmail.secureserver.net.', {})
+    create_record(params[:domain], 'www', 'CNAME', "#{params[:domain]}.", {})
+  end
+
+  #Creates NetworkSolution MX and CNAME records
+  def create_networksolutions_records
+    create_mx_record(params[:domain], '', '0', "inbound.#{params[:domain]}.netsolmail.net.", {} )
+
+    create_record(params[:domain], 'mail', 'CNAME', "mail.#{params[:domain]}.netsolmail.net.", {})
+    create_record(params[:domain], 'mail', 'CNAME', "smtp.#{params[:domain]}.netsolmail.net.", {})
+    create_record(params[:domain], 'www', 'CNAME', "#{params[:domain]}.", {})
+  end
+
+  # Creates the default ANAME and CNAME records for all zones
+  def create_default_records(mx_type)
+    create_record(params[:domain], '', 'ANAME', 'www', {})
+    create_record(params[:domain], 'www', 'CNAME', 'lb.moxiworks.com', {})
+  end
+
+  # Determines which MX records to create
+  def create_additional_records
+    case params[:mx_type]
+    when 'gmail'
+      create_gmail_records
+    when 'godaddy'
+      create_godaddy_records
+    when 'networksolutions'
+      create_networksolutions_records
+    when 'none'
+      create_record(params[:domain], 'www', 'CNAME', "#{params[:domain]}.", {})
+    else
+      return
+    end
+  end
+
 
   # Creates the agent zone in DME
   def create_zone
-    #create_domain
-    #create_default_records
-    create_gmail_records
+    create_domain
+    create_default_records
   end
 end
